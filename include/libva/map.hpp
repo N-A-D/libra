0904f3467d1fe6c0025740533f63893538029260
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-05-21 14:36:34 -0400

    Added headers for all planned containers

---
+++ include/libva/map.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <initializer_list>
+#include "detail/extract_key.hpp"
+#include "detail/ordered_container.hpp"
+
+namespace va {
+
+	template <
+		class Key,
+		class Value,
+		class Compare = std::less<Key>,
+		class Allocator = std::allocator<std::pair<Key, Value>>
+	> class map 
+		: detail::ordered_container<Key, Value, Compare, Allocator, detail::select1st<std::pair<Key, Value>>> 
+	{
+		using base_type = detail::ordered_container
+							<
+								Key, 
+								Value, 
+								Compare, 
+								Allocator, 
+								detail::select1st<std::pair<Key, Value>>
+							>;
+	public:
+		
+		using typename base_type::key_type;
+		using typename base_type::value_type;
+		using typename base_type::size_type;
+		using typename base_type::difference_type;
+		using typename base_type::key_compare;
+		using typename base_type::value_compare;
+		using typename base_type::allocator_type;
+		using typename base_type::reference;
+		using typename base_type::const_reference;
+		using typename base_type::pointer;
+		using typename base_type::const_pointer;
+		using typename base_type::iterator;
+		using typename base_type::const_iterator;
+		using typename base_type::reverse_iterator; 
+		using typename base_type::const_reverse_iterator;
+
+	};
+
+	template <
+		class Key,
+		class Value,
+		class Compare = std::less<Key>,
+		class Allocator = std::allocator<std::pair<Key, Value>>
+	> class multimap
+		: detail::ordered_container<Key, Value, Compare, Allocator, detail::select1st<std::pair<Key, Value>>>
+	{
+		using base_type = detail::ordered_container
+							<
+								Key,
+								Value,
+								Compare,
+								Allocator,
+								detail::select1st<std::pair<Key, Value>>
+							>;
+	public:
+
+		using typename base_type::key_type;
+		using typename base_type::value_type;
+		using typename base_type::size_type;
+		using typename base_type::difference_type;
+		using typename base_type::key_compare;
+		using typename base_type::value_compare;
+		using typename base_type::allocator_type;
+		using typename base_type::reference;
+		using typename base_type::const_reference;
+		using typename base_type::pointer;
+		using typename base_type::const_pointer;
+		using typename base_type::iterator;
+		using typename base_type::const_iterator;
+		using typename base_type::reverse_iterator;
+		using typename base_type::const_reverse_iterator;
+
+	};
+
+}

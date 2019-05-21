92ea06e28164aec0fcbc4f8beaefd0f0fcd6fb1d
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-05-21 12:41:48 -0400

    is_unique requires a sorted range

--- tests/utility.hpp
+++ tests/utility.hpp
@@ -2,6 +2,7 @@
 
 #include <random>
 #include <vector>
+#include <algorithm>
 
 const int N = 100;
 
@@ -9,6 +10,7 @@ std::mt19937 gen{ std::random_device{}() };
 
 template <class RndIt>
 bool is_unique(RndIt first, RndIt last) {
+	assert(std::is_sorted(first, last));
 	if (first == last)
 		return true;
 	auto next = first + 1;
@@ -22,6 +24,7 @@ bool is_unique(RndIt first, RndIt last) {
 
 template <class RndIt, class BinaryPred>
 bool is_unique(RndIt first, RndIt last, BinaryPred p) {
+	assert(std::is_sorted(first, last, p));
 	if (first == last)
 		return true;
 	auto next = first + 1;

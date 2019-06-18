e47988af24a1f7821b551e9c660aeb0565bac8a1
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-06-18 00:39:52 -0400

    Replaced deque::increment/decrement. Provided implementation for previously unimplemented member functions. Assignment ops remain are still unimplemented

--- include/libva/deque.hpp
+++ include/libva/deque.hpp
@@ -8,7 +8,6 @@
 
 namespace va {
 	namespace detail {
-
 		template <
 			class Deque,
 			bool IsConst = false
@@ -102,7 +101,7 @@ namespace va {
 			deque_iterator& operator++() {
 				assert(is_valid());
 				assert(m_it && "Increment out of bounds!");
-				m_data->increment(m_it);
+				m_it = m_data->next(m_it);
 				if (m_it == m_data->m_tail)
 					m_it = nullptr;
 				return *this;
@@ -121,7 +120,7 @@ namespace va {
 				assert(m_it != m_data->m_head && "Decrement out of bounds!");
 				if (m_it == nullptr)
 					m_it = m_data->m_tail;
-				m_data->decrement(m_it);
+				m_it = m_data->prev(m_it);
 				return *this;
 			}
 
@@ -139,7 +138,7 @@ namespace va {
 					return *this;
 				else if (n > 0) {
 					assert(m_data->end() - *this >= n && "Increment out of bounds!");
-					m_it = m_data->increment_by(m_it, n);
+					m_it = m_data->next(m_it, n);
 					if (m_it == m_data->m_tail)
 						m_it = nullptr;
 					return *this;
@@ -163,7 +162,7 @@ namespace va {
 					assert(*this - m_data->begin() >= n && "Decrement out of bounds!");
 					if (m_it == nullptr)
 						m_it = m_data->m_tail;
-					m_it = m_data->decrement_by(m_it, n);
+					m_it = m_data->prev(m_it, n);
 					return *this;
 				}
 				else {
@@ -206,7 +205,6 @@ namespace va {
 			}
 
 		};
-		
 	}
 
 	template <
@@ -353,7 +351,7 @@ namespace va {
 		const_reference operator[](size_type index) const {
 			assert(!empty());
 			assert(valid_index(index));
-			return *increment_by(m_head, index);
+			return *next(m_head, index);
 		}
 
 		reference front() { 
@@ -385,11 +383,11 @@ namespace va {
 		const_iterator cend() const noexcept { return const_iterator(this, nullptr); }
 
 		reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
-		const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(end()); }
+		const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(cend()); }
 		const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(cend()); }
 
 		reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
-		const_reverse_iterator rend() const noexcept { return const_reverse_iterator(begin()); }
+		const_reverse_iterator rend() const noexcept { return const_reverse_iterator(cbegin()); }
 		const_reverse_iterator crend() const noexcept { return const_reverse_iterator(cbegin()); }
 
 		// capacity
@@ -398,35 +396,56 @@ namespace va {
 		size_type size() const noexcept { return m_size; }
 		size_type max_size() const noexcept { return alloc_traits::max_size(m_alloc); }
 		size_type capacity() const noexcept { return m_limit - m_data; }
-		
-		///////////////////////////////////////////////////////////////////////////////////////
-		//									TODO                                             //
-		void shrink_to_fit();
-
+		void shrink_to_fit() { release_unused_memory(); }
 
 		// modifiers
+
 		void clear() noexcept { uninitialize(); }
 
-		///////////////////////////////////////////////////////////////////////////////////////
-		//									TODO                                             //
-
-		iterator insert(const_iterator pos, const value_type& value);
-		iterator insert(const_iterator pos, value_type&& value);
-		iterator insert(const_iterator pos, size_type n, const value_type& value);
+		iterator insert(const_iterator pos, const value_type& value) { return emplace(pos, value); }
+		iterator insert(const_iterator pos, value_type&& value) { return emplace(pos, std::move(value)); }
+		iterator insert(const_iterator pos, size_type n, const value_type& value) {
+			if (n == 0)
+				return end();
+			iterator ret = emplace(pos, value);
+			--n;
+			while (n != 0) {
+				ret = emplace(ret, value);
+				--n;
+			}
+			return ret;
+		}
 
 		template <class InIt>
-		iterator insert(const_iterator pos, InIt first, InIt last);
+		iterator insert(const_iterator pos, InIt first, InIt last) {
+			if (first == last)
+				return end();
+			InIt it = last;
+			iterator ret = emplace(pos, --*it);
+			while (it != first)
+				ret = emplace(ret, --*it);
+			return ret;
+		}
 
-		iterator insert(const_iterator pos, std::initializer_list<value_type> list);
+		iterator insert(const_iterator pos, std::initializer_list<value_type> list) {
+			return insert(pos, list.begin(), list.end());
+		}
 
 		template <class... Args>
-		iterator emplace(const_iterator pos, Args&&... args);
+		iterator emplace(const_iterator pos, Args&&... args) 
+		{ return emplace_at(pos, std::forward<Args>(args)...); }
 
-		iterator erase(const_iterator pos);
-		iterator erase(const_iterator first, const_iterator last);
-
-		//                                                                                   //
-		///////////////////////////////////////////////////////////////////////////////////////
+		iterator erase(const_iterator pos) { return remove_at(pos); }
+		iterator erase(const_iterator first, const_iterator last) {
+			assert(last != cend() && first != cend());
+			if (first == last)
+				return end();
+			const_iterator it = last;
+			iterator ret = erase(--it);
+			while (it != first)
+				ret = erase(--it);
+			return ret;
+		}
 
 		void push_front(const T& value) { append_front(value); }
 		void push_front(T&& value) { append_front(std::move(value)); }
@@ -476,6 +495,7 @@ namespace va {
 				std::swap(m_head, other.m_head);
 				std::swap(m_tail, other.m_tail);
 				std::swap(m_size, other.m_size);
+				std::swap(m_alloc, other.m_alloc);
 			}
 		}
 
@@ -492,36 +512,7 @@ namespace va {
 
 	private:
 
-		// helpers
-
-		bool valid_index(size_type index) const noexcept {
-			return index < m_size;
-		}
-		
-		template <class Pointer>
-		void increment(Pointer& p) const noexcept {
-			if (++p == m_limit)
-				p = m_data;
-		}
-
-		template <class Pointer>
-		Pointer increment_by(Pointer p, difference_type n) const noexcept {
-			return p + ((m_limit - p) > n ? n : n - (m_limit - m_data));
-		}
-
-		template <class Pointer>
-		void decrement(Pointer& p) const noexcept {
-			if (p == m_data)
-				p = m_limit;
-			--p;
-		}
-
-		template <class Pointer>
-		Pointer decrement_by(Pointer p, difference_type n) const noexcept {
-			return p - ((p - m_data) > n ? n : n - (m_limit - m_data));
-		}
-
-		// Memory management
+		// memory management
 		
 		void initialize() {
 			m_size = 0;
@@ -597,49 +588,144 @@ namespace va {
 			}
 			size_type new_size = m_size;
 			uninitialize();
-			m_data = new_data;
-			m_limit = m_data + new_cap;
 			m_size = new_size;
-			m_head = m_data;
+			m_data = m_head = new_data;
 			m_tail = m_data + m_size;
+			m_limit = m_data + new_cap;
 		}
 
-		// Element construction
+		void release_unused_memory() {
+			if (!full()) {
+				pointer new_data = alloc_traits::allocate(m_alloc, m_size);
+				if (m_tail > m_head) {
+					if constexpr (std::is_move_constructible_v<T>)
+						std::uninitialized_move(m_head, m_tail, new_data);
+					else
+						std::uninitialized_copy(m_head, m_tail, new_data);
+				}
+				else {
+					if constexpr (std::is_move_constructible_v<T>) {
+						pointer tmp = std::uninitialized_move(m_head, m_limit, new_data);
+						std::uninitialized_move(m_data, m_tail, tmp);
+					}
+					else {
+						pointer tmp = std::uninitialized_copy(m_head, m_limit, new_data);
+						std::uninitialized_copy(m_data, m_tail, tmp);
+					}
+				}
+				size_type new_size = m_size;
+				uninitialize();
+				m_size = new_size;
+				m_data = m_head = new_data;
+				m_limit = m_tail = m_data + new_size;
+			}
+		}
+
+		// helpers
+
+		bool valid_index(size_type index) const noexcept {
+			return index < m_size;
+		}
+
+		template <class Pointer>
+		Pointer next(Pointer p, difference_type n = 1) const noexcept {
+			return p + ((m_limit - p) > n ? n : n - (m_limit - m_data));
+		}
+
+		template <class Pointer>
+		Pointer prev(Pointer p, difference_type n = 1) const noexcept {
+			return p - ((p - m_data) >= n ? n : n - (m_limit - m_data));
+		}
+
+		// element construction
+
+		template <class... Args>
+		void construct_element(pointer pos, Args&&... args) {
+			alloc_traits::construct(m_alloc, pos, std::forward<Args>(args)...);
+		}
+
+		void destroy_element(pointer p) {
+			alloc_traits::destroy(m_alloc, p);
+		}
+
+		template <class... Args>
+		iterator emplace_at(const_iterator pos, Args&&... args) {
+			if (pos == cend()) {
+				append_back(std::forward<Args>(args)...);
+				return std::prev(end());
+			}
+			if (pos == cbegin()) {
+				append_front(std::forward<Args>(args)...);
+				return begin();
+			}
+			difference_type off_from_front = pos - cbegin();
+			difference_type off_from_back = cend() - pos;
+			if (off_from_front < off_from_back) {
+				append_front(std::forward<Args>(args)...);
+				reverse_iterator it = rbegin() + off_from_back;
+				it = std::rotate(it, std::prev(rend()), rend());
+				return it.base();
+			}
+			else {
+				append_back(std::forward<Args>(args)...);
+				iterator it = begin() + off_from_front;
+				it = std::rotate(it, std::prev(end()), end());
+				return it;
+			}
+		}
+
+		iterator remove_at(const_iterator pos) {
+			assert(pos != cend());
+			if (std::next(pos) == end())
+				remove_back();
+			if (pos == cbegin())
+				remove_front();
+			difference_type off_from_front = pos - cbegin();
+			difference_type off_from_back = cend() - pos;
+			if (off_from_front < off_from_back) { 
+				reverse_iterator it = rbegin() + off_from_back;
+				it = std::rotate(it, it + 1, rend());
+				remove_front();
+				return it.base();
+			}
+			else {
+				iterator it = begin() + off_from_front;
+				it = std::rotate(it, it + 1, end());
+				remove_back();
+				return it;
+			}
+		}
 
 		template <class... Args>
 		void append_front(Args&&... args) {
 			if (empty() || full())
 				grow();
-			if (m_head == m_data)
-				m_head = m_limit;
-			alloc_traits::construct(m_alloc, --m_head, std::forward<Args>(args)...);
+			m_head = prev(m_head);
+			construct_element(m_head, std::forward<Args>(args)...);
 			++m_size;
+		}
+
+		void remove_front() {
+			assert(!empty());
+			destroy_element(m_head);
+			m_head = next(m_head);
+			--m_size;
 		}
 
 		template <class... Args>
 		void append_back(Args&&... args) {
 			if (empty() || full())
 				grow();
-			if (m_tail == m_limit)
-				m_tail = m_data;
-			alloc_traits::construct(m_alloc, m_tail++, std::forward<Args>(args)...);
+			construct_element(m_tail, std::forward<Args>(args)...);
+			m_tail = next(m_tail);
 			++m_size;
-		}
-
-		void remove_front() {
-			assert(!empty());
-			--m_size;
-			alloc_traits::destroy(m_alloc, m_head++);
-			if (m_head == m_limit)
-				m_head = m_data;
 		}
 
 		void remove_back() {
 			assert(!empty());
+			m_tail = prev(m_tail);
+			destroy_element(m_tail);
 			--m_size;
-			alloc_traits::destroy(m_alloc, --m_tail);
-			if (m_tail == m_data)
-				m_tail == m_limit;
 		}
 
 	};
